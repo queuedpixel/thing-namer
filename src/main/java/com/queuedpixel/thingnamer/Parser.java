1fc0b2f8fae1d2fedfacda23e0661d329d0a318b
Queued Pixel <git@queuedpixel.com>
2019-01-06 08:53:54 -0800

    Added first version of dictionary parser that prints standard input.

---
+++ src/main/java/com/queuedpixel/thingnamer/Parser.java
@@ -0,0 +1,49 @@
+/*
+
+thing-namer : Combine Words Together to Form a Random Name
+
+Copyright (c) 2019 Queued Pixel <git@queuedpixel.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+package com.queuedpixel.thingnamer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Parser
+{
+    public static void main( String[] args ) throws Exception
+    {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ));
+        String line = reader.readLine();
+        while ( line != null )
+        {
+            if ( builder.length() > 0 ) builder.append( " " );
+            builder.append( line );
+            line = reader.readLine();
+        }
+
+        String contents = builder.toString();
+        System.out.println( contents );
+    }
+}
